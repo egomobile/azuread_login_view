0734cf08a4e8dea64223d79e6f68112c9400055a
Marcel Kloubert <marcel.kloubert@e-go-mobile.com>
2022-06-28 21:41:31 +0200

    initial commit

---
+++ example/android/app/src/main/kotlin/com/example/azuread_login_view_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.azuread_login_view_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/azuread_login_view.dart
@@ -0,0 +1,22 @@
+// This file is part of the azuread_login_view distribution.
+// Copyright (c) Next.e.GO Mobile SE, Aachen, Germany (https://e-go-mobile.com/)
+//
+// azuread_login_view is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, version 3.
+//
+// azuread_login_view is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+library azuread_login_view;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
